82189a8adf716cae0cda0365b63d9c9fcdf9bd69
Ruben Ramirez <rramirezv@ultrasist.com.mx>
2022-01-02 12:36:57 -0600

    Update Inicio.java

--- src/paquete1/Inicio.java
+++ src/paquete1/Inicio.java
@@ -2,16 +2,8 @@ package paquete1;
 
 public class Inicio {
 
-	//Mi corazon lloro y eslla no nontesto
-	//Con quien estesss
-	//se sgrega el cambio del sds
-	//Paco gabriel de anda
-	//Dile que atienda, que si ha marchado entonces adiossss
-	//Niegamelo
-	//AI
-	
-	//CAMBIO DE PUTAS
+	//Depuracion toño
 
-	//CAMBIO DE PUTAS2
+
 }
 
