87af56d7fc87e80381563aa90e0dfa3359e52985
Ruben Ramirez <rramirezv@ultrasist.com.mx>
2022-01-01 22:45:35 -0600

    Update Inicio.java

--- src/paquete1/Inicio.java
+++ src/paquete1/Inicio.java
@@ -4,4 +4,13 @@ public class Inicio {
 
 	//dos enamorados QU ENUNCA SE QUIEREM QUE SE HAN ENTREGADO AL AMAOR
 	//dos enamoradosss chingas a tu madreees
+	//dos enamorados QU ENUNCA SE QUIEREM QUE SE HAN ENTREGADO AL AMAOR
+	//dos enamoradosss chingas a tu madreees
+	//dos enamorados QU ENUNCA SE QUIEREM QUE SE HAN ENTREGADO AL AMAOR
+	//dos enamoradosss chingas a tu madreees
+	//dos enamorados QU ENUNCA SE QUIEREM QUE SE HAN ENTREGADO AL AMAOR
+	//dos enamoradosss chingas a tu madreees
+	//dos enamorados QU ENUNCA SE QUIEREM QUE SE HAN ENTREGADO AL AMAOR
+	//dos enamoradosss chingas a tu madreees
+
 }
