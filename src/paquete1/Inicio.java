6815ea799db404b26c92dbf7fe7b48210a16a76c
Ruben Ramirez <rramirezv@ultrasist.com.mx>
2022-01-02 12:16:15 -0600

    Update Inicio.java

--- src/paquete1/Inicio.java
+++ src/paquete1/Inicio.java
@@ -2,12 +2,7 @@ package paquete1;
 
 public class Inicio {
 
-	//se sgrega el cambio del sds
-	//Cambio de agrega el buki
-	//Carga de controlador
-	//Kabuki
-	//Chuiqilla bonit
-	//Mi corazon lloro
+	//Mi corazon lloro y eslla no nontesto
 
 }
 
