5a393976b0dc39ef10069bb458b9d1895da2c27a
Ruben Ramirez <rramirezv@ultrasist.com.mx>
2022-01-02 12:00:29 -0600

    ok

--- src/paquete1/Inicio.java
+++ src/paquete1/Inicio.java
@@ -2,17 +2,6 @@ package paquete1;
 
 public class Inicio {
 
-	//dos enamorados QU ENUNCA SE QUIEREM QUE SE HAN ENTREGADO AL AMAOR
-	//dos enamoradosss chingas a tu madreees
-	//dos enamorados QU ENUNCA SE QUIEREM QUE SE HAN ENTREGADO AL AMAOR
-	//dos enamoradosss chingas a tu madreees
-	//dos enamorados QU ENUNCA SE QUIEREM QUE SE HAN ENTREGADO AL AMAOR
-	//dos enamoradosss chingas a tu madreees
-	//dos enamorados QU ENUNCA SE QUIEREM QUE SE HAN ENTREGADO AL AMAOR
-	//dos enamoradosss chingas a tu madreees
-	//dos enamorados QU ENUNCA SE QUIEREM QUE SE HAN ENTREGADO AL AMAOR
-	//dos enamoradosss chingas a tu madreees
-	
 	//se sgrega el cambio del sds
 
 }
